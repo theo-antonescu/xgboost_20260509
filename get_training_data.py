e06a6ffaaade03566c8ce2df022f8fe9dacd4f59
theo-antonescu <theo.antonescu@gmail.com>
2026-05-09 20:17:17 -0400

    Added function to get diabetes data and convert to polars LazyFrame for training/processing

--- get_training_data.py
+++ get_training_data.py
@@ -1,11 +1,27 @@
 import polars as pl
 import os
 import sklearn
-from sklearn.datasets import make_regression
+from sklearn.datasets import load_diabetes
 
+pl.Config(set_tbl_cols=15,
+          set_tbl_rows=100)
 
-def generate_regression_data():
-    # Generate synthetic regression data
-    X, y = make_regression(n_samples=10000, n_features=10, noise=0.2, random_state=123)
-    return X, y
+def get_diabetes_data() -> pl.LazyFrame:
+    # Get sample data
+    diabetes_data = load_diabetes()
+    # Show info about data and columns
+    print(diabetes_data['DESCR'])
+    # Convert data to LF and assign column names
+    diabetes_lf = (
+        pl.LazyFrame(
+            data=diabetes_data['data'],
+            schema=diabetes_data['feature_names']
+        )
+        # Add the target/y variable to be predicted
+        .with_columns(pl.Series('target', diabetes_data['target']))
+    )
+    return diabetes_lf
 
+if __name__ == "__main__":
+    print(get_diabetes_data().collect())
+    
