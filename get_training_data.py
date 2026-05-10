0a332de57c7deaca4d6405b82550dfe25339e624
theo-antonescu <theo.antonescu@gmail.com>
2026-05-09 20:54:04 -0400

    Added ldl to hdl ratio interaction term

--- get_training_data.py
+++ get_training_data.py
@@ -11,17 +11,37 @@ def get_diabetes_data() -> pl.LazyFrame:
     diabetes_data = load_diabetes()
     # Show info about data and columns
     print(diabetes_data['DESCR'])
+    print(f'Sample size: {(sample_size := len(diabetes_data['data']))}')
     # Convert data to LF and assign column names
     diabetes_lf = (
         pl.LazyFrame(
             data=diabetes_data['data'],
             schema=diabetes_data['feature_names']
         )
-        # Add the target/y variable to be predicted
-        .with_columns(pl.Series('target', diabetes_data['target']))
+        .rename(
+            {
+            's1': 'tc',
+            's2': 'ldl',
+            's3': 'hdl', 
+            's4': 'tch',
+            's5': 'ltg',
+            's6': 'glu'
+            }
+        )
+        .with_columns(
+            # Apply some transformations to provided features (this ratio has little meaning as the provided columns are standardized)
+            (pl.col('ldl')/pl.col('hdl')).alias('ldl_to_hdl_ratio'),
+            # Add the target/y variable to be predicted
+            pl.Series('target', diabetes_data['target']),
+                      )
+        .with_columns(
+            # Center and divide by std so this column matches others
+            ((pl.col("ldl_to_hdl_ratio") - pl.col("ldl_to_hdl_ratio").mean()
+              )/(pl.col("ldl_to_hdl_ratio").std()*(sample_size**0.5))
+             ).alias('ldl_to_hdl_ratio')
+        )
     )
     return diabetes_lf
 
 if __name__ == "__main__":
     print(get_diabetes_data().collect())
-    
